5cfa698de3f28b063f95b674dfd75bfe6eb5f8a4
James Garcia <colossus9@github.com>
2022-05-23 13:12:38 -0500

    Parsing the github repo url

--- vars/detectRepoLanguages.groovy
+++ vars/detectRepoLanguages.groovy
@@ -6,10 +6,10 @@ String[] call() {
     println('Begin detect repository language procedure')
 
     // https://docs.github.com/en/rest/repos/repos#list-repository-languages
-    
-    sh 'printenv'
-    //REPO_LANGUAGES_URL = TBD
+    URI uri = new URI(GIT_URL)
+    GITHUB_REPO_URL = uri.getHost()
 
+    println('GitHub Repo URL: ' + GITHUB_REPO_URL)
 
 
     GH_CLI_INSTALLED_VERSION = "1.0.0"
