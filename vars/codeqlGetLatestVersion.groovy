38a1cf310eccd7ff0ced0d008135120bdba5af9b
James Garcia <colossus9@github.com>
2022-05-23 09:54:24 -0500

    Using var

--- vars/codeqlGetLatestVersion.groovy
+++ vars/codeqlGetLatestVersion.groovy
@@ -6,7 +6,7 @@ String call(String version = 'latest') {
     * @return The value of latest version of the CodeQL bundle
     */
     
-    releasesUrl = readJSON(file: 'resources/codeql.json').releasesUrl
+    releasesUrl = 'https://api.github.com/repos/github/codeql-action/releases/latest'
 
     latestVersion = sh(script: "curl --silent '${releasesUrl}' | grep '\"tag_name\":'", returnStdout: true).toString().trim()
     latestVersion = latestVersion.split('\"')[3].trim()
--- vars/codeqlInstall.groovy
+++ vars/codeqlInstall.groovy
@@ -1,18 +1,16 @@
-String call(String version = 'latest') {
+String call(String version) {
     /**
-    * Gets the latest CodeQL bundle version.
+    * Installs the requested CodeQL version
     *
     * @param version The version of the CodeQL bundle to install
     * @return The latest version of the CodeQL bundle
     */
 
-    // If we specify 'latest', then go get the latest release
-    if (version == 'latest') {
-        println('Version specified: ' + version)
+    println('Version requested to install: ' + version)
 
-    // If we don't specify 'latest', check which version we need to get
-    } else {
-        // TODO: get a specific version
-        println('Need to implement: Get specific version. Version specified: ' + version)
-    }
+    CODEQL_BIN_PATH="/codeql"
+    CODEQL_CLI_ARCHIVE="/codeql/codeql.tar.gz"
+    CODEQL_CLI_PATH="$CODEQL_BIN_PATH/bin/codeql"
+
+    
 }
