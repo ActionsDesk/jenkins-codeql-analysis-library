7cc6020a08704c6872cc8ee8547b0a8ee7074747
James Garcia <colossus9@github.com>
2022-05-23 09:50:18 -0500

    Using resources file

--- vars/codeqlGetLatestVersion.groovy
+++ vars/codeqlGetLatestVersion.groovy
@@ -6,7 +6,7 @@ String call(String version = 'latest') {
     * @return The value of latest version of the CodeQL bundle
     */
     
-    releasesUrl = readJSON(file: 'resources/com/github/codeql.json').releasesUrl
+    releasesUrl = readJSON(file: 'resources/codeql.json').releasesUrl
 
     latestVersion = sh(script: "curl --silent '${releasesUrl}' | grep '\"tag_name\":'", returnStdout: true).toString().trim()
     latestVersion = latestVersion.split('\"')[3].trim()
