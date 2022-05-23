0e215d62c3b000c799c491026694fa4c74304310
James Garcia <colossus9@github.com>
2022-05-23 10:36:40 -0500

    Fixed path

--- vars/codeqlDownload.groovy
+++ vars/codeqlDownload.groovy
@@ -9,7 +9,7 @@ def call(String version, String os = 'linux') {
 
     CODEQL_BIN_PATH = './codeql'
     CODEQL_CLI_ARCHIVE = './codeql/codeql.tar.gz'
-    CODEQL_CLI_PATH = CODEQL_BIN_PATH + './bin/codeql'
+    CODEQL_CLI_PATH = CODEQL_BIN_PATH + '/bin/codeql'
 
     // https://github.com/github/codeql-action/releases
     switch (os) {
