d4a786a48a72558739f8bf37aeedea4039bd1579
James Garcia <colossus9@github.com>
2022-05-23 09:23:05 -0500

    Split string

--- vars/codeqlLatestVersion.groovy
+++ vars/codeqlLatestVersion.groovy
@@ -8,7 +8,9 @@ String call(String version = 'latest') {
 
     println('Get CodeQL Version specified: ' + version)
 
-    latestVersion = sh(script: "curl --silent 'https://api.github.com/repos/github/codeql-action/releases/latest' | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/'", returnStdout: true).toString().trim()
+    latestVersion = sh(script: "curl --silent 'https://api.github.com/repos/github/codeql-action/releases/latest' | grep '\"tag_name\":'", returnStdout: true).toString().trim()
+    latestVersion = latestVersion.split(':')[1].trim()
+
 
     println('Retrieved the Latest version number: ' + latestVersion)
 
