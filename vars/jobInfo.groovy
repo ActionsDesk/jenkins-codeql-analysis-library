281797785baa4e855e9f18c425778999bf45398e
James Garcia <colossus9@github.com>
2022-05-22 23:45:58 -0500

    Using call for jobInfo

--- vars/jobInfo.groovy
+++ vars/jobInfo.groovy
@@ -1,9 +1,10 @@
-String getBuildUser() {
+String call() {
   /**
    * Provides the username of the user who kicked off the build
    *
    * @return The username of the person who kicked off the build
    */
 
+  println*("Getting user id...")
   return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
 }
