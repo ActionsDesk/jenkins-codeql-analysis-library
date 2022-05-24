d745956876e45b439e91e56cc3e5b5e3c3467e34
James Garcia <colossus9@github.com>
2022-05-23 22:14:00 -0500

    Codeql Create Command

--- vars/codeqlInit.groovy
+++ vars/codeqlInit.groovy
@@ -15,9 +15,77 @@ def call() {
     (gitHost, gitOrgRepo) = getRepoInfo.parseGitUrl()
 
     println("GitHub Repository URL: ${GIT_URL}")
-    println("Github Ref: ${GIT_COMMIT}")
+    println("GitHub Ref: ${GIT_COMMIT}")
+
+    // Get repo languages
+    def compiledLanguages = []
+    def interpretedLanguages = []
+
+    (compiledLanguages, interpretedLanguages) = getRepoInfo.getLanguages("${GITHUB_CREDENTIAL_ID}")
+
+    // Warn if there were no languages detected
+    if (compiledLanguages.isEmpty() && interpretedLanguages.isEmpty()) {
+        println("WARNING: No languages detected")
+    }
 
 
+    String codeqlGitHubRepository = gitOrgRepo.replace("/", "_")
 
+    // Work on detected compiled languages
+    if (!compiledLanguages.isEmpty()) {
 
+        for (String language in compiledLanguages) {
+            println("Init compiled language: ${language}")
+            String codeqlDatabase = getCodeqlDatabase(language, codeqlGitHubRepository)
+            deleteOldDatabase(codeqlDatabase)
+
+            codeqlCreate = codeqlCreateCommand(language)
+            println("CodeQL create command: ${codeqlCreate}")
+
+            // TODO
+        }
+
+    }
+
+    // Work on detected interpreted languages
+    if (!interpretedLanguages.isEmpty()) {
+
+        for (String language in interpretedLanguages) {
+            println("Init interpreted language: ${language}")
+            String codeqlDatabase = getCodeqlDatabase(language, codeqlGitHubRepository)
+            deleteOldDatabase(codeqlDatabase)
+
+            codeqlCreate = codeqlCreateCommand(language)
+            println("CodeQL create command: ${codeqlCreate}")
+
+            // TODO
+        }
+
+    }
+}
+
+String getCodeqlDatabase(String language, String codeqlGitHubRepository) {
+    /**
+    * Gets the CodeQL database for the given language and repo
+    */
+
+    String codeqlDatabase = "${CODEQL_DATABASES}/${language}-${codeqlGitHubRepository}"
+    return codeqlDatabase
+}
+
+def deleteOldDatabase(String codeqlDatabase) {
+    /**
+    * Deletes the old database
+    */
+
+    sh(script: "rm -rf ${codeqlDatabase}", returnStdout: true)
+}
+
+String codeqlCreateCommand(String language) {
+    /**
+    * Generates the the CodeQL create command for the given language
+    */
+
+    String command = "codeql database create --language=${language}}"
+    return command
 }
