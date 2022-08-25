a304585938e8ed165ee55bb5657e6fea0c6cbf2b
David Wiggs <107562400+david-wiggs@users.noreply.github.com>
2022-08-24 20:49:55 -0500

    Update codeqlGetLatestVersion.groovy

--- vars/codeqlGetLatestVersion.groovy
+++ vars/codeqlGetLatestVersion.groovy
@@ -4,18 +4,13 @@ String call() {
     *
     * @return The value of latest version of the CodeQL bundle
     */
-    
-    latestVersion = pwsh(returnStdout: true, script: Invoke-RestMethod -Method 'Get' -Uri 'https://api.github.com/repos/github/codeql-action/releases/latest' -ContentType 'application/json')
-                         
-    
-    
 
-//     releasesUrl = 'https://api.github.com/repos/github/codeql-action/releases/latest'
+    releasesUrl = 'https://api.github.com/repos/github/codeql-action/releases/latest'
 
-//     latestVersion = sh(script: "curl --silent '${releasesUrl}' | grep '\"tag_name\":'", returnStdout: true).toString().trim()
-//     latestVersion = latestVersion.split('\"')[3].trim()
+    latestVersion = sh(script: "curl --silent '${releasesUrl}' | grep '\"tag_name\":'", returnStdout: true).toString().trim()
+    latestVersion = latestVersion.split('\"')[3].trim()
 
-//     println('Retrieved the latest CodeQL bundle version number: ' + latestVersion)
+    println('Retrieved the latest CodeQL bundle version number: ' + latestVersion)
 
     return latestVersion
 }
